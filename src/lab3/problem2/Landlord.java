2ac891baf628c6a3d6dc8d48cdc2d12ac9d18467
thaiphan <thaiphan2712@gmail.com>
2025-04-03 13:28:38 -0500

    update

--- src/lab3/problem2/Landlord.java
+++ src/lab3/problem2/Landlord.java
@@ -8,16 +8,16 @@ public class Landlord {
     private String name;
     private List<Building> building = new ArrayList<>();
 
-    Landlord(String name){
+    Landlord(String name) {
         this.name = name;
     }
 
-    public void addBuilding(String buildnum, Double cost){
+    public void addBuilding(String buildnum, Double cost) {
         Building build = new Building(buildnum, this, cost);
         building.add(build);
     }
 
-    public List<Building> getListBuilding(){
+    public List<Building> getListBuilding() {
         return building;
     }
 
@@ -37,15 +37,15 @@ public class Landlord {
         this.building = building;
     }
 
-    public Double calculateProfit(){
+    public Double calculateProfit() {
         Double sum = 0.0;
         Double cost = 0.0;
-        for (Building build:building){
-            cost+=build.getBuildingCost();
-            for (Apartment apart:build.getAparts()){
-                sum+=apart.getRent();
+        for (Building build : building) {
+            cost += build.getBuildingCost();
+            for (Apartment apart : build.getAparts()) {
+                sum += apart.getRent();
             }
         }
-        return sum-cost;
+        return sum - cost;
     }
 }
--- src/lab3/problem2/Main.java
+++ src/lab3/problem2/Main.java
@@ -8,13 +8,14 @@ public class Main {
         landlord.addBuilding("B002", 123.98);
         landlord.addBuilding("B003", 500.00);
 
-        for(Building build:landlord.getBuilding()){
-            if(build.getBuildingnum().equals("B001")){
+        for (Building build : landlord.getBuilding()) {
+            if (build.getBuildingnum().equals("B001")) {
                 build.addApartment("A001", 600.00);
                 build.addApartment("A002", 1000.00);
             }
         }
 
-        System.out.println("The profit that landlord "+landlord.getName()+" achieve for one month is "+landlord.calculateProfit());
+        System.out.println("The profit that landlord " + landlord.getName() + " achieve for one month is "
+                + landlord.calculateProfit());
     }
 }
