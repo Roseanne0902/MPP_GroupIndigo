6efadd85fb830bc6820ab6799308bfc2dd9fb6dc
Selamawit <selamawitzere16@gmail.com>
2025-04-02 23:17:34 -0500

    prob3 A and B solution

---
+++ lab3/problem3/src/prob3A/CylinderA.java
@@ -0,0 +1,23 @@
+public class CylinderA {
+
+    private double radius;
+     double height;
+
+
+    public CylinderA(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public CylinderA() {
+
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double computeVolume(){
+        return 3.14* getRadius()* getRadius()* height;
+    }
+}
