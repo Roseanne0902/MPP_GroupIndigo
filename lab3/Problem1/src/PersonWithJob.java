1f4bae7e0f73ab24d9f892054564eff5ff571396
uyengit <uhuong571@gmail.com>
2025-04-03 00:53:49 -0500

    1&2

---
+++ lab3/Problem1/src/PersonWithJob.java
@@ -0,0 +1,39 @@
+public class PersonWithJob {
+
+    private Person person;
+
+    private double salary;
+    PersonWithJob(String n, double s) {
+        this.person = new Person(n);
+        this.salary = s;
+    }
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getName(){
+        return person.getName();
+    }
+    public Person getPerson(){
+        return person;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "PersonWithJob{" +
+                "person=" + person +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object aPersonWithJob) {
+        if(aPersonWithJob == null) return false;
+        if(!(aPersonWithJob instanceof PersonWithJob)) return false;
+        PersonWithJob p = (PersonWithJob)aPersonWithJob;
+        boolean isEqual = this.getName().equals(p.getName()) &&
+                this.getSalary()==p.getSalary();
+        return isEqual;
+    }
+
+}
