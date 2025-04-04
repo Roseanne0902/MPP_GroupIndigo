1b8ce542a0a7e82f1bea75b5316880dfd1bc6cff
thaiphan <thaiphan2712@gmail.com>
2025-04-04 15:13:56 -0500

    update lab 5 prob 2

---
+++ src/lab5/problem2/extpackage/Main.java
@@ -0,0 +1,24 @@
+package lab5.problem2.extpackage;
+
+import lab5.problem2.Customer;
+import lab5.problem2.Order;
+
+import java.time.LocalDate;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Customer cust = new Customer("Bob");
+		Order order = Order.newOrder(cust, LocalDate.now());
+		order.addItem("Shirt");
+		order.addItem("Laptop");
+
+		order = Order.newOrder(cust, LocalDate.now());
+		order.addItem("Pants");
+		order.addItem("Knife set");
+
+		System.out.println(cust.getOrders());
+	}
+}
+
+		
