ad8233b95fb5aae3be708f54983358d823ad7efd
uyengit <uhuong571@gmail.com>
2025-04-06 00:25:40 -0500

    prob1

--- src/lab5/problem2/CustOrderFactory.java
+++ src/lab5/problem2/CustOrderFactory.java
@@ -3,7 +3,7 @@ package lab5.problem2;
 import java.time.LocalDate;
 
 public class CustOrderFactory {
-    private CustOrderFactory() {} // prevent instantiation
+    private CustOrderFactory() {}
 
     public static Customer createCustomer(String name) {
         return new Customer(name);
