c5edda27b15b061d23aa925c322f1909dc7b3cb5
thaiphan <thaiphan2712@gmail.com>
2025-04-07 10:44:48 -0500

    update lab

--- src/lab4/problem3/Paycheck.java
+++ src/lab4/problem3/Paycheck.java
@@ -3,24 +3,24 @@ package lab4.problem3;
 public final class Paycheck {
 
     private final double grossPay;
-    private final double ficaRate = 0.23;
+    private final double FICA_RATE = 0.23;
     private final double fica;
-    private final double stateRate = 0.05;
+    private final double STATE_RATE = 0.05;
     private final double state;
-    private final double localRate = 0.01;
+    private final double LOCAL_RATE = 0.01;
     private final double local;
-    private final double medicareRate = 0.03;
+    private final double MEDICARE_RATE = 0.03;
     private final double medicare;
-    private final double socialSecurityRate = 0.075;
+    private final double SOCIAL_SECURITY_RATE = 0.075;
     private final double socialSecurity;
 
     public Paycheck(double grossPay) {
         this.grossPay = grossPay;
-        this.fica = grossPay * ficaRate;
-        this.state = grossPay * stateRate;
-        this.local =  grossPay *  localRate;
-        this.medicare = grossPay *  medicareRate;
-        this.socialSecurity = grossPay *  socialSecurityRate;
+        this.fica = grossPay * FICA_RATE;
+        this.state = grossPay * STATE_RATE;
+        this.local =  grossPay * LOCAL_RATE;
+        this.medicare = grossPay * MEDICARE_RATE;
+        this.socialSecurity = grossPay * SOCIAL_SECURITY_RATE;
     }
 
     public double getNetPay() {
