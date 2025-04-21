3c980d3985f4cd99fa79dc18a15136ea3f3bb1a9
Selamawit <selamawitzere16@gmail.com>
2025-04-20 19:18:19 -0500

    TEMP- WILL BE MOVED

---
+++ src/project/updated/CategoryManager.java
@@ -0,0 +1,15 @@
+package project.updated;
+
+
+public class CategoryManager {
+    private final CategoryRepository categoryRepository = new CategoryRepository();
+
+    public String addCategory(Category category) {
+        try {
+            categoryRepository.addCategory(category);
+            return "Category added successfully";
+        } catch (Exception e) {
+            return "Error adding category";
+        }
+    }
+}
